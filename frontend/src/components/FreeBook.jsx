5638e8076af6089a325d4b83a6dcbf534adaa0cc
vishnusureshas <vishnuasuresh2000@gmail.com>
2024-04-24 20:38:37 +0530

    Frontend!

---
+++ frontend/src/components/FreeBook.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import list from "../../public/list.json";
+import Slider from "react-slick";
+import Cards from "./Cards";
+
+const FreeBook = () => {
+  const filterData = list.filter((data) => data.category === "Music");
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offer Courses</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {filterData.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FreeBook;
